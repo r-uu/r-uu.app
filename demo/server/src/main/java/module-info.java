861c6a38c850a70fa03f85211f00121100b47593
r-uu <roger.wegner@web.de>
2024-06-04 16:11:05 +0200

    minor

---
+++ demo/server/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module ruu.app.demo.server
+{
+//	exports de.ruu.lib.cdi.common;
+//	opens   de.ruu.lib.cdi.common;
+
+	requires microprofile.metrics.api;
+	requires jakarta.jakartaee.api;
+//	requires jakarta.ws.rs;
+//	requires lombok;
+//requires slf4j.api;
+//	requires org.slf4j;
+}
